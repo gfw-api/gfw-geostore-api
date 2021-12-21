28636da6c682d588a71b4f2a8349f5105e6198b8
Alicia <alicia.arenzana@gmail.com>
2021-12-21 16:24:07 +0100

    finish developing an antimeridian fix

--- app/test/e2e/utils/test.constants.js
+++ app/test/e2e/utils/test.constants.js
@@ -1,67 +1,83 @@
 const DEFAULT_GEOJSON = {
     type: 'FeatureCollection',
-    features: [{
-        type: 'Feature',
-        properties: {},
-        geometry: {
-            type: 'MultiPoint',
-            coordinates: [
-                [
-                    14.26438308756265,
-                    14.062500000000002
+    features: [
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'MultiPoint',
+                coordinates: [
+                    [14.26438308756265, 14.062500000000002],
+                    [5.266007882805498, 2.8125],
+                    [44.84029065139799, 16.523437500000004],
                 ],
-                [
-                    5.266007882805498,
-                    2.8125
+            },
+        },
+    ],
+};
+
+const ANTIMERIDIAN_GEOJSON_WRONG = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Polygon',
+                coordinates: [
+                    [
+                        [177.066650390625, -20.663626054152797],
+                        [-173.759765625, -20.663626054152797],
+                        [-173.759765625, -13.218555949175457],
+                        [177.066650390625, -13.218555949175457],
+                        [177.066650390625, -20.663626054152797],
+                    ],
                 ],
-                [
-                    44.84029065139799,
-                    16.523437500000004
-                ],
-            ]
-        }
-    }],
+            },
+        },
+    ],
 };
 
 const ANTIMERIDIAN_GEOJSON = {
     type: 'FeatureCollection',
-    features: [{
-        type: 'Feature',
-        properties: {},
-        geometry: {
-            type: 'Polygon',
-            coordinates: [
-                [
-                    [
-                        177.066650390625,
-                        -20.663626054152797
+    features: [
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'MultiPolygon',
+                coordinates: [
+                    [[
+                        [176.515420, -34.184289],
+                        [180, -34.184289],
+                        [180, -27.592584],
+                        [176.515420, -27.592584],
+                        [176.515420, -34.184289],
                     ],
-                    [
-                        -173.759765625,
-                        -20.663626054152797
                     ],
-                    [
-                        -173.759765625,
-                        -13.218555949175457
+                    [[
+                        [-180, -34.184289],
+                        [-173, -34.184289],
+                        [-173, -27.592584],
+                        [-180, -27.592584],
+                        [-180, -34.184289],
                     ],
-                    [
-                        177.066650390625,
-                        -13.218555949175457
-                    ],
-                    [
-                        177.066650390625,
-                        -20.663626054152797
                     ]
-                ]
-            ]
+                ],
+            },
         }
-    }],
+    ],
 };
 
-const MOCK_RESULT_CARTODB = [{
-    geojson: '{"type":"MultiPolygon","coordinates":[[[[7.4134,43.7346],[7.4396,43.7492],[7.4179,43.7226],[7.4095,43.7299],[7.4134,43.7346]]]]}',
-    area_ha: 235.490994944,
-    name: 'Monaco'
-}];
+const MOCK_RESULT_CARTODB = [
+    {
+        geojson:
+            '{"type":"MultiPolygon","coordinates":[[[[7.4134,43.7346],[7.4396,43.7492],[7.4179,43.7226],[7.4095,43.7299],[7.4134,43.7346]]]]}',
+        area_ha: 235.490994944,
+        name: 'Monaco',
+    },
+];
 
-module.exports = { DEFAULT_GEOJSON, ANTIMERIDIAN_GEOJSON, MOCK_RESULT_CARTODB };
+module.exports = {
+    DEFAULT_GEOJSON, ANTIMERIDIAN_GEOJSON, ANTIMERIDIAN_GEOJSON_WRONG, MOCK_RESULT_CARTODB
+};
